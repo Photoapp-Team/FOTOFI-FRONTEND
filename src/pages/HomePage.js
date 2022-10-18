92fde529cb9f84de81dfd38d3f85fa6a0aa4fda1
Marco Castañeda <106879979+deathmarcus@users.noreply.github.com>
2022-10-18 12:39:13 -0500

    Test card

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,12 @@
 import { useContext } from "react";
+import RecipeReviewCard from "../components/Cards/PhotographerCard/PhotographerCard";
 import { UserContext } from "../contexts/UserContext";
 
 const Homepage = () => {
   const user = useContext(UserContext);
   return (
     <>
+      <RecipeReviewCard />
       {!user ? <p>Loading...</p> : <p>Bienvenido {user.userName} a Fotofi</p>}
     </>
   );
