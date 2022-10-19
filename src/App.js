98ea5911b6b89b72743a2f3e611a7b6c507dfa40
luised2301 <luised2301@gmail.com>
2022-10-18 23:14:21 -0500

    card footers and edit mode added

--- src/App.js
+++ src/App.js
@@ -2,10 +2,8 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import UserContextProvider from "./contexts/UserContext";
 import Homepage from "./pages/HomePage";
-import ServiceCard from "./components/Cards/ServiceCard/ServiceCard";
 import { createTheme, ThemeProvider, styled } from "@mui/material/styles";
 import { green, purple, yellow, grey } from "@mui/material/colors";
-import PhotographerCard from "./components/Cards/PhotographerCard";
 import "@fontsource/raleway"; // Defaults to weight 400.
 
 const theme = createTheme({
--- src/components/Cards/ServiceCard/ServiceCard.jsx
+++ src/components/Cards/ServiceCard/ServiceCard.jsx
@@ -6,11 +6,22 @@ import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { CardActionArea } from "@mui/material";
 import Skeleton from "@mui/material/Skeleton";
+import ServiceCardFooter from "../ServiceCardFooter";
 
-export default function ServiceCard({ service, img, isLoaded }) {
+export default function ServiceCard({
+  service,
+  img,
+  isLoaded,
+  withFooter,
+  minPrice,
+  editMode,
+}) {
   return (
     <>
-      <Card sx={{ maxWidth: 250, borderRadius: 2 }} elevation={4}>
+      <Card
+        sx={{ maxWidth: 250, borderRadius: 2, minWidth: 202, margin: 2 }}
+        elevation={4}
+      >
         <CardActionArea sx={{ maxWidth: 300, padding: ".5rem" }}>
           {isLoaded ? (
             <CardMedia
@@ -38,6 +49,15 @@ export default function ServiceCard({ service, img, isLoaded }) {
             )}
           </CardContent>
         </CardActionArea>
+        {withFooter ? (
+          <ServiceCardFooter
+            editMode={editMode}
+            isLoaded={isLoaded}
+            minPrice={minPrice}
+          />
+        ) : (
+          <></>
+        )}
       </Card>
     </>
   );
