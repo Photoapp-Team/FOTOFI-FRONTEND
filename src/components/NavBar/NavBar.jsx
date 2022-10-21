c242d36538468aeb64d60e401bbbffbc9932b22a
Jose De Anda <josdandar@gmail.com>
2022-10-20 19:00:01 -0700

    cleaning comments

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -49,16 +49,3 @@ export default function ButtonAppBar() {
   );
 }
 
-{/* <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-</IconButton>
-
-<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            FOTOFI
-          </Typography> */}
