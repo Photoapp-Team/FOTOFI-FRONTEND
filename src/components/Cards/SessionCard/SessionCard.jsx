665a7867ac86740355e149eaf9e7d6d70f939d54
luised2301 <luised2301@gmail.com>
2022-10-26 23:16:32 -0500

    ServiceCard relocation and session container created

--- src/components/Cards/SessionCard/SessionCard.jsx
+++ src/components/Cards/SessionCard/SessionCard.jsx
@@ -12,14 +12,8 @@ import Divider from "@mui/material/Divider";
 import useFetchUser from "../../../services/useFetchUser";
 
 export default function SessionCard({ data }) {
-  const { REACT_APP_API_ENDPOINT } = process.env;
-
-  //   const fetchUser = useFetchUser(`${REACT_APP_API_ENDPOINT}/users/${userId}`);
-  //   const fetchPhotographer = useFetchUser(
-  //     `${REACT_APP_API_ENDPOINT}/users/${userId}`
-  //   );
-
-  const isLoaded = true;
+  const lastStatus = Object.keys(data.status).length - 2;
+  const status = Object.keys(data.status)[lastStatus];
   return (
     <>
       <Card
@@ -27,113 +21,68 @@ export default function SessionCard({ data }) {
         sx={{ maxWidth: 450, borderRadius: 2, minWidth: 202, margin: 2 }}
         elevation={4}
       >
+        <Typography
+          variant="subtitle"
+          component="div"
+          children={`Sesion Para:`}
+          align="left"
+          boxSizing="content-box"
+          sx={{ p: 0.75 }}
+        />
         <CardHeader
           className="cardHeader"
-          font
           sx={{ p: 0.75 }}
-          avatar={
-            isLoaded ? (
-              <Avatar alt={`Sesion: ${data._id}`} src={"userData.profilePic"} />
-            ) : (
-              <Skeleton
-                animation="wave"
-                variant="circular"
-                width={40}
-                height={40}
-              />
-            )
-          }
-          title={
-            isLoaded ? (
-              `Sesion: ${data._id}`
-            ) : (
-              <Skeleton variant="text" width="60%" sx={{ fontSize: ".9rem" }} />
-            )
-          }
-          subheader={
-            isLoaded ? (
-              "location"
-            ) : (
-              <Skeleton variant="text" width="40%" sx={{ fontSize: ".9rem" }} />
-            )
-          }
+          avatar={<Avatar alt={`Sesion: ${data._id}`} src={"userData.profilePic"} />}
+          title={`Sesion: ${data._id}`}
+          subheader={"location"}
         />
-
+        <Divider />
+        <Typography
+          variant="subtitle"
+          component="div"
+          children="Fecha programada:"
+          align="left"
+          boxSizing="content-box"
+          sx={{ p: 0.75 }}
+        />
+        <Typography
+          variant="subtitle"
+          component="div"
+          children={data.startDate}
+          align="left"
+          boxSizing="content-box"
+          sx={{ p: 0.75 }}
+        />
+        <Divider />
         <CardActionArea sx={{ maxWidth: 300, padding: ".5rem" }}>
-          {isLoaded ? (
-            <Typography
-              className="cardTitle"
-              variant="h5"
-              component="div"
-              children={"el servicio aqui"}
-              align="center"
-              boxSizing="content-box"
-            />
-          ) : (
-            <Skeleton variant="text" sx={{ fontSize: "1.5rem" }} />
-          )}
+          <Typography
+            variant="body"
+            component="div"
+            children={`Estatus:${status}`}
+            align="center"
+            boxSizing="content-box"
+          />
 
           <CardContent sx={{ pb: 0 }}>
             <List>
               <ListItem>
-                <ListItemText
-                  primary={
-                    isLoaded ? (
-                      `para usuario ${data.userId[0]}`
-                    ) : (
-                      <Skeleton
-                        variant="text"
-                        width="60%"
-                        sx={{ fontSize: ".9rem" }}
-                      />
-                    )
-                  }
-                />
+                <ListItemText primary={`para usuario ${data.userId[0]}`} />
               </ListItem>
               <ListItem>
-                <ListItemText
-                  primary={
-                    isLoaded ? (
-                      `Del fotografo ${data.photographerId[0]}`
-                    ) : (
-                      <Skeleton
-                        variant="text"
-                        width="60%"
-                        sx={{ fontSize: ".9rem" }}
-                      />
-                    )
-                  }
-                />
+                <ListItemText primary={`Del fotografo ${data.photographerId[0]}`} />
               </ListItem>
               <ListItem>
-                <ListItemText
-                  primary={
-                    isLoaded ? (
-                      `Texto`
-                    ) : (
-                      <Skeleton
-                        variant="text"
-                        width="60%"
-                        sx={{ fontSize: ".9rem" }}
-                      />
-                    )
-                  }
-                />
+                <ListItemText primary={`Texto`} />
               </ListItem>
             </List>
-
-            {isLoaded ? (
-              <Typography
-                className="cardTitle"
-                variant="h5"
-                component="div"
-                children={`creada el ${data.starDate}`}
-                align="center"
-                boxSizing="content-box"
-              />
-            ) : (
-              <Skeleton variant="text" sx={{ fontSize: "1.5rem" }} />
-            )}
+            <Typography
+              className="cardTitle"
+              variant="h5"
+              component="div"
+              children={`Estatus:${status}`}
+              align="center"
+              boxSizing="content-box"
+            />
           </CardContent>
         </CardActionArea>
       </Card>
