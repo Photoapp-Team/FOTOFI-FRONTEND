c04e7bdf68dd6dc739077b0e096fa8043100da05
Gio <giovannigonzalezdelgado25@gmail.com>
2022-11-01 21:59:16 -0600

    the update user service is created

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ import NewSessionPage from "./pages/NewSessionPage/NewSessionPage";
 import SelectionPage from "./pages/SelectionPage";
 import SessionPage from "./pages/SessionPage/SessionPage";
 import RatePage from "./pages/RatePage/RatePage";
+import EditProfile from "./pages/EditProfilePage/EditProfilePage"
 
 const theme = createTheme({
   typography: {
@@ -67,6 +68,7 @@ const App = () => {
                 <Route path="/Selection/:id" element={<SelectionPage />}></Route>
                 <Route path="/Session/:id" element={<SessionPage />}></Route>
                 <Route path="/Rate/:id" element={<RatePage />}></Route>
+                <Route path="/EditProfile" element={<EditProfile />}></Route>
               </Routes>
             </>
           </div>
--- src/components/Forms/EditProfileForm/EditProfileForm.jsx
+++ src/components/Forms/EditProfileForm/EditProfileForm.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
-// import Box from "@mui/material/Box";
-// import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
 import "./EditProfile.css";
 import { Formik, Form, validateYupSchema } from "formik";
-import CustomInput from "../Inputs/CustomInput";
-import { editSchema } from "../schemas";
-//import Button from "../Button/Button";
-import { updateUser } from "../../services/updateUser";
+import CustomInput from "../../Inputs/CustomInput";
+import { editSchema } from "../../schemas/index";
+import Button from "../../Inputs/Button/Button";
+import { updateUser } from "../../../services/updateUser";
+
 
 const onSubmit = async (values, actions) => {
   updateUser(values);
-  console.log(values);
   actions.resetForm();
 };
 
 const EditProfileForm = () => {
   return (
     <div className="form-contain">
-      {/* <Box
+      <Box
         sx={{
           display: "flex",
           flexWrap: "wrap",
@@ -28,66 +28,66 @@ const EditProfileForm = () => {
           },
         }}
       >
-        <Paper elevation={3}> */}
-      <div className="">
-        <h1 className="edit-profile-title">Edita tú perfil</h1>
-        <Formik
-          initialValues={{ lastname: "", name: "", email: "" }}
-          validationSchema={editSchema}
-          onSubmit={onSubmit}
-        >
-          {({ isSubmitting }) => (
-            <Form className="container-edit-profile">
-              <br />
-              <CustomInput
-                label="Nombre"
-                name="name"
-                type="text"
-              //placeholder="Ingresa tu nombre"
-              />
-              <CustomInput
-                label="Apellido"
-                name="lastname"
-                type="text"
-              //placeholder="Ingresa tu apellido"
-              />
-              <CustomInput
-                label="Email"
-                name="email"
-                type="email"
-              //placeholder="Ingresa tu nuevo email"
-              />
-              <CustomInput
-                label="Ubicación"
-                name="location"
-                type="text"
-              //placeholder="Ingresa tu nueva ubicación"
-              />
-              <CustomInput
-                label="Número telefonico"
-                name="phoneNumber"
-                type="number"
-              //placeholder="Ingresa número telefónico"
-              />
-              <CustomInput
-                label="Página Web"
-                name="webPage"
-                type="text"
-              //placeholder="Ingresa página WEB"
-              />
-              {/* <Button
+        <Paper elevation={3}>
+          <div className="">
+            <h1 className="edit-profile-title">Edita tú perfil</h1>
+            <Formik
+              initialValues={{ lastname: "", name: "", email: "" }}
+              validationSchema={editSchema}
+              onSubmit={onSubmit}
+            >
+              {({ isSubmitting }) => (
+                <Form className="container-edit-profile">
+                  <br />
+                  <CustomInput
+                    label="Nombre"
+                    name="name"
+                    type="text"
+                  //placeholder="Ingresa tu nombre"
+                  />
+                  <CustomInput
+                    label="Apellido"
+                    name="lastname"
+                    type="text"
+                  //placeholder="Ingresa tu apellido"
+                  />
+                  <CustomInput
+                    label="Email"
+                    name="email"
+                    type="email"
+                  //placeholder="Ingresa tu nuevo email"
+                  />
+                  <CustomInput
+                    label="Ubicación"
+                    name="location"
+                    type="text"
+                  //placeholder="Ingresa tu nueva ubicación"
+                  />
+                  <CustomInput
+                    label="Número telefonico"
+                    name="phoneNumber"
+                    type="number"
+                  //placeholder="Ingresa número telefónico"
+                  />
+                  <CustomInput
+                    label="Página Web"
+                    name="webPage"
+                    type="text"
+                  //placeholder="Ingresa página WEB"
+                  />
+                  <Button
                     disabled={isSubmitting}
                     type="submit"
                     text={"Submit"}
                     name={"Editar"}
                     className={"button-edit-profile"}
-                  /> */}
-            </Form>
-          )}
-        </Formik>
-      </div>
-      {/* </Paper>
-      </Box> */}
+                  />
+                </Form>
+              )}
+            </Formik>
+          </div>
+        </Paper>
+      </Box>
     </div>
   )
 }
