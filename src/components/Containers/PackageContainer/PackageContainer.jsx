9f8355b8b8e12fe031a8294cf9c941caa643cba5
Gio <giovannigonzalezdelgado25@gmail.com>
2022-11-04 10:16:30 -0600

    function is started to render payments

--- src/components/Containers/PackageContainer/PackageContainer.jsx
+++ src/components/Containers/PackageContainer/PackageContainer.jsx
@@ -5,10 +5,16 @@ import Container from "@mui/material/Container";
 import useFetchPackage from "../../../services/FetchServices/useFetchPackage";
 import PackageGalleryCard from "../../Cards/PackageGalleryCard/PackageGalleryCard";
 import PackageInfoCard from "../../Cards/PackageInfoCard";
+import { useParams } from "react-router-dom";
+import useFetchUser from "../../../services/useFetchUser";
 
 export default function PackageContainer({ data }) {
   const packageData = data.data;
   const fetchSuccess = !data.loading;
+  const { REACT_APP_API_ENDPOINT } = process.env
+  const URL = `${REACT_APP_API_ENDPOINT}/users/${packageData.photographerId}`
+  const userData = useFetchUser(URL)
+  console.log(userData)
   return (
     <Container maxWidth="md">
       <Grid container spacing={2} justifyContent="center" alignItems="center">
@@ -18,10 +24,10 @@ export default function PackageContainer({ data }) {
               isLoaded={fetchSuccess}
               name="Luis"
               photos={packageData.displayPhotos}
-              //!MANDAR INFO DESDE LA PAGINA ANTERIOR
-              // profilePic=""
-              // location=""
-              // photographerId=""
+            //!MANDAR INFO DESDE LA PAGINA ANTERIOR
+            // profilePic=""
+            // location=""
+            // photographerId=""
             />
           ) : (
             <PackageGalleryCard isLoaded={fetchSuccess} sx={1} />
