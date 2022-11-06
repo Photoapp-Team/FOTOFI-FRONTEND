4b24775d1db5af02be2293880b0b1cd167e9c31a
luised2301 <luised2301@gmail.com>
2022-11-06 14:25:23 -0600

    Create session alerts

--- src/pages/NewSessionPage/NewSessionPage.jsx
+++ src/pages/NewSessionPage/NewSessionPage.jsx
@@ -1,28 +1,30 @@
 import "./NewSessionPage.css";
 import { useParams } from "react-router-dom";
-import { Grid, Container, Alert } from "@mui/material";
-import React, { useState, useEffect } from "react";
+import { Grid, Container } from "@mui/material";
+import React, { useState } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
-import { Formik, Form, Field } from "formik";
+import { Formik, Form } from "formik";
 import CustomInput from "../../components/Inputs/CustomInput";
 import Button from "../../components/Inputs/Button/Button";
 import { createSession } from "../../services/createSession";
 import { useNavigate } from "react-router-dom";
 import { useUser } from "../../contexts/UserContext";
-import Typography from "@mui/material/Typography";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+import { dateFormater } from "../../services/dateFormater";
 
 const NewSessionPage = () => {
+  const MySwal = withReactContent(Swal);
   const params = useParams();
-  const { isUserLoggedIn, user } = useUser();
+  const { user } = useUser();
   const queryParams = new URLSearchParams(window.location.search);
   const photographerId = queryParams.get("photographerId");
   const [startDate, onChange] = useState(new Date());
   const token = localStorage.getItem("token");
   const navigate = useNavigate();
-  const [alert, setAlert] = useState(null);
-  const [alertMessage, setAlertMessage] = useState("");
-  const onSubmit = (values, actions) => {
+
+  const onSubmit = async (values, actions) => {
     if (token) {
       const sessionData = {
         ...values,
@@ -30,33 +32,33 @@ const NewSessionPage = () => {
         package: params.id,
         photographerId,
       };
-      console.log(user._id);
-      createSession(sessionData);
-      actions.resetForm();
-      setAlertMessage("sesion creada con exito");
-      setAlert("success");
-      const timeout = setTimeout(() => {
-        navigate(`/Profile/${user._id}`);
-      }, 3000);
+      const date = dateFormater(sessionData.startDate);
+
+      Swal.fire({
+        title: `Quieres crear una session para el ${date}`,
+        icon: `question`,
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Confirmar",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          createSession(sessionData).then((sessionResult) => {
+            navigate(`/Profile/${user._id}`);
+          });
+        }
+      });
     } else {
-      setAlertMessage("Tienes que estar logeado para crear session");
-      setAlert("error");
+      Swal.fire({
+        title: `Tienes que estar logeado para cerar una session`,
+        icon: `error`,
+      });
     }
   };
 
   return (
     <Container className="mainContainer" maxWidth="md" sx={{ p: 4 }}>
-      {alert ? <Alert severity={alert}>{alertMessage}</Alert> : <></>}
-      <Typography
-        sx={{ px: 4, pt: 2 }}
-        className="filterTitle"
-        variant="h4"
-        component="div"
-        children="Selecciona una fecha para agendar tu sessión"
-        align="center"
-        boxSizing="content-box"
-      />
-      <Grid container spacing={2} justifyContent="center" alignItems="center" height="inherit">
+      <Grid container spacing={2} justifyContent="center" alignItems="center">
         <Grid item xs={12} md={8}>
           <Calendar onChange={onChange} value={startDate} />
         </Grid>
--- src/services/createSession.js
+++ src/services/createSession.js
@@ -1,8 +1,12 @@
-const { REACT_APP_API_ENDPOINT } = process.env;
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
 
+const { REACT_APP_API_ENDPOINT } = process.env;
 const USER_URL = `${REACT_APP_API_ENDPOINT}/sessions/`;
 
 export const createSession = async (sessionData) => {
+  const MySwal = withReactContent(Swal);
+
   const token = localStorage.getItem("token");
   let userId = "";
   if (token) {
@@ -19,6 +23,21 @@ export const createSession = async (sessionData) => {
     body: JSON.stringify(sessionData),
   });
   const data = await response.json();
+  console.log(data);
+  if (data.success === true) {
+    MySwal.fire({
+      title: <strong>Session creada con exito! </strong>,
+      icon: `success`,
+    });
+    return data;
+  }
+  if (data.success !== true) {
+    MySwal.fire({
+      title: <strong>La session no pudo ser creada! </strong>,
+      icon: `error`,
+    });
+    return data;
+  }
 
   return data;
 };
