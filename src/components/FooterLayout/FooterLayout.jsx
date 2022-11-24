75b841bd34a6cc6fc242e70341f17220a42f7b29
araceli <araceli.romero@gmail.com>
2022-11-23 23:03:38 -0600

    Fix Fixicons

--- src/components/FooterLayout/FooterLayout.jsx
+++ src/components/FooterLayout/FooterLayout.jsx
@@ -2,20 +2,16 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import "./FooterLayout.css";
 import { Typography } from '@mui/material';
-import Facebook from "../../images/facebook.png";
-import Instagram from "../../images/instagramlogo.png";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
 
 function FooterLayout () {
   return (
-    <div className= "footer-layout">
+    <>
       <Box className="footerContainer"  position="static" sx={{display: "flex", flexDirection:"column", backgroundColor: '#f1f1f1', p: 3,}}>
-        <Box sx={{gap:2, display: "flex"}} >
-          <a href="/">
-            <img src={Facebook} className="facebook" />
-          </a>
-          <a href="/">
-            <img src={Instagram} className="instagram" />
-          </a>
+        <Box sx={{gap:2, display: "flex"}}> 
+          <FacebookIcon sx={{ fontSize: 30 }} />
+          <InstagramIcon sx={{ fontSize: 30 }} />
         </Box>
         <Box>
           <Typography align="center" variant="body1" sx={{ marginBottom: '1rem', lineHeight: "2rem" }}>
@@ -31,7 +27,7 @@ function FooterLayout () {
           </Typography>
         </Box>
       </Box>
-    </div>
+    </>
   );
 }
 
