136ac55499fc2b28ccefd632183a54040f9f34f2
Marco Castañeda <marco.dcc@gmail.com>
2022-11-25 15:36:55 -0600

    Fix: generalIssues, changed the session card and layout on profile page

--- src/components/Cards/SessionCard/SessionCard.jsx
+++ src/components/Cards/SessionCard/SessionCard.jsx
@@ -7,10 +7,11 @@ import { CardActionArea } from "@mui/material";
 import Avatar from "@mui/material/Avatar";
 import CardHeader from "@mui/material/CardHeader";
 import Divider from "@mui/material/Divider";
-import useFetchBasicUser from "../../../services/FetchServices/useFetchBasicUser";
+import useFetchBasicUser from "../../../services/useFetchBasicUser";
 import useFetchPackage from "../../../services/FetchServices/useFetchPackage";
 import { dateFormater } from "../../../services/dateFormater";
 import { statusFormater } from "../../../services/statusFormater";
+import { Box } from "@mui/system";
 
 export default function SessionCard({ data }) {
   const clientData = useFetchBasicUser(data.userId);
@@ -26,7 +27,7 @@ export default function SessionCard({ data }) {
     <>
       <Card
         className={`sessioncard-${lastStatus}`}
-        sx={{ maxWidth: 450, borderRadius: 2, minWidth: 202, margin: 2 }}
+        sx={{ width: 220, borderRadius: 2, minWidth: 202, margin: 2, height: 300 }}
         elevation={4}
       >
         <CardActionArea sx={{ maxWidth: 300, padding: ".5rem" }} href={`/session/${data._id}`}>
@@ -35,12 +36,12 @@ export default function SessionCard({ data }) {
             component="div"
             children={
               packageData?.data
-                ? `Servicio de fotografía ${packageData.data.serviceCategory}`
+                ? `Paquete de ${packageData.data.serviceCategory.toLowerCase()}`
                 : "pendiente"
             }
             align="center"
             boxSizing="content-box"
-            sx={{ p: 1 }}
+            sx={{ p: 1, textAlign: "initial" }}
           />
           <Divider />
           <Typography
@@ -56,13 +57,12 @@ export default function SessionCard({ data }) {
             sx={{ p: 0.75 }}
             avatar={<Avatar alt={`Sesion: ${data.username}`} src={clientData.data?.profilePic} />}
             title={clientData.data?.username}
-            subheader={clientData.data?.location.city}
           />
           <Divider />
           <Typography
             variant="subtitle"
             component="div"
-            children="Fecha programada:"
+            children="Fecha solicitada:"
             align="left"
             boxSizing="content-box"
             sx={{ p: 0.75 }}
@@ -71,38 +71,40 @@ export default function SessionCard({ data }) {
             variant="subtitle"
             component="div"
             children={fecha}
-            align="center"
+            align="initial"
             boxSizing="content-box"
-            sx={{ p: 0.75 }}
+            sx={{ p: 0.75, fontWeight: 700 }}
           />
           <Divider />
-
-          <Typography
-            variant="subtitle"
-            component="div"
-            children="Ubicacion:"
-            align="left"
-            boxSizing="content-box"
-            sx={{ p: 0.75 }}
-          />
-          <Typography
-            variant="subtitle"
-            component="div"
-            children={data.location}
-            align="center"
-            boxSizing="content-box"
-            sx={{ p: 0.75 }}
-          />
+          <Box sx={{ minHeight: "62px" }}>
+            <Typography
+              variant="subtitle"
+              component="div"
+              children="Ubicacion:"
+              align="left"
+              boxSizing="content-box"
+              sx={{ p: 0.75 }}
+            />
+            <Typography
+              variant="subtitle"
+              component="div"
+              children={data?.location ? `${data.location.toUpperCase()}` : ""}
+              align="initial"
+              boxSizing="content-box"
+              sx={{ p: 0.75, fontWeight: 700 }}
+            />
+          </Box>
           <Divider />
 
           <CardContent sx={{ pb: 0 }}>
             <Typography
               className="cardTitle"
-              variant="h5"
+              variant="h6"
               component="div"
               children={lastStatus}
-              align="center"
+              align="initial"
               boxSizing="content-box"
+              sx={{ fontWeight: 700 }}
             />
           </CardContent>
         </CardActionArea>
